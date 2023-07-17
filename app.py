b79b106e3186fc0b9b3c6d40c16b86efa6c82bba
Stéphane D <stephanediraimondo@gmail.com>
2023-07-17 23:09:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/api/hello/")
 def hello():
     return "Flask inside Docker!!"
 
+@app.route('/api/meteo/')
+def meteo():
+    dictionnaire = {
+        'type': 'Prévision de température',
+        'valeurs': [24, 24, 25, 26, 27, 28],
+        'unite': "degrés Celcius"
+    }
+    return jsonify(dictionnaire)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
